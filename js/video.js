02638f8a61eaf0b887defc80b1dbde4841c46114
Mia Schaftel <78377452+miascha@users.noreply.github.com>
2021-11-15 22:17:57 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,23 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play()
+	document.getElementById("volume").innerHTML=video.volume*100+"%";
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause()
+	});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log(video.playbackRate);
+	video.playbackRate-=0.05;
+});
 

a5b9631855ce9422aa191c64e5042a74482d40af
Mia Schaftel <78377452+miascha@users.noreply.github.com>
2021-11-15 22:43:03 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -21,3 +21,45 @@ document.querySelector("#slower").addEventListener("click", function() {
 	video.playbackRate-=0.05;
 });
 
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log(video.playbackRate);
+	video.playbackRate+=0.05;
+});
+
+document.querySelector('#skip').addEventListener("click", function(){
+	console.log(video.currentTime);
+	video.currentTime+=15;
+
+	if (video.currentTime>=video.duration){
+		video.currentTime=0;
+		console.log(video.currentTime);
+	}
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Mute Video");
+
+	if(video.muted){
+		video.muted = false;
+		document.getElementById("mute").innerHTML="Mute"
+	}
+
+	else{
+		video.muted=true;
+		document.getElementById("mute").innerHTML="Unmute"
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log(this.value);
+	video.volume=this.value/100;
+	document.getElementById("volume").innerHTML=this.value+"%"
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add('oldSchool')
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove('oldSchool')
+});
